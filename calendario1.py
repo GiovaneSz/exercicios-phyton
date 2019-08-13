96d661c68f529daf5b36d6eb3eadfc584fa6095d
giovane <giovanesantlima@gamil.com>
2019-08-13 17:33:09 -0300

    exercicios de python

---
+++ calendario1.py
@@ -0,0 +1,25 @@
+numero = int(input('digite um numero de 1 a 7 '))
+
+if numero == 1 :
+    print('numero 1 é segunda')
+
+elif numero == 2:
+    print('numero 2 é terça')
+
+elif numero == 3:
+    print('numero 3 é quarta')
+
+elif numero == 4:
+    print('numero 4 é quinta')
+
+elif numero == 5:
+    print('numero 5 é sexta')
+
+elif numero == 6:
+    print('numero 6 é sabado')
+
+elif numero == 7:
+    print('numero 7 é domingo')
+
+else :
+    print('numero incorreto')
---
+++ funcionario1.py
@@ -0,0 +1,8 @@
+nome = float (input ("digite seu salario"))
+hora = float (input ("digite quantas horas trabalhadas"))
+valor = float (input ("digite seu salario por hora"))
+
+salario = hora * valor 
+print ("o seu salario é:" + str(salario))
+
+
